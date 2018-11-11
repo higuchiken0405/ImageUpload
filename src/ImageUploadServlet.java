dc2a6192f01e77e6e600e5aaee22dacc126f7d53
kenhiguchi0405 <higuchiken0405@gmail.com>
2018-11-11 18:35:56 +0900

    フォームからのファイルを受け取りディレクトリに書き込むサーブレットを追加

---
+++ src/ImageUploadServlet.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet("/upload")
+@MultipartConfig(location="/tmp")
+public class ImageUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ImageUploadServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Part part = request.getPart("file");
+        String name = this.getFileName(part);
+        part.write(getServletContext().getRealPath("/upload") + "/" + name);
+        request.getSession().setAttribute("name", name);
+        response.sendRedirect("result.jsp");
+	}
+
+    private String getFileName(Part part) {
+        String name = null;
+        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+            if (dispotion.trim().startsWith("filename")) {
+                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+                name = name.substring(name.lastIndexOf("\\") + 1);
+                break;
+            }
+        }
+        return name;
+    }
+
+}
